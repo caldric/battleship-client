68c5ffeffbc33caabca7a754ed7820d16b00c942
caldric <cvaldric@gmail.com>
2020-07-22 00:17:50 -0400

    Create basic React app that renders all data ID

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import React, { useEffect, useState } from 'react';
+// import NewForm from './components/NewForm';
 
-function App() {
+// Configuration
+const API = {
+  local: 'http://localhost:8080',
+  deployment: 'https://gentle-temple-22561.herokuapp.com',
+  index: 'index',
+};
+const baseURL =
+  process.env && process.env.NODE_ENV === 'development'
+    ? API.local
+    : API.deployment;
+
+// Functional react component
+const App = () => {
+  // State Hook: [stateVariable, stateVariableSetter] = useState(initialState);
+  const [data, setData] = useState([]);
+
+  // Fetch API data function
+  const getData = async () => {
+    // Send GET request to API
+    const url = `${baseURL}/${API.index}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Set state
+    setData(data);
+  };
+
+  // UseEffect Hook: call getData() on mount
+  useEffect(() => {
+    console.log('Triggered!');
+    getData();
+  }, []);
+
+  // Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sample H1</h1>
+      <ul>
+        {data.map((datum) => (
+          <li key={datum._id}>{datum._id}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
+// Component export
 export default App;
