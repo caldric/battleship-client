cfc5b194a1672152b604d74d7bb4d6d20bdca82b
caldric <cvaldric@gmail.com>
2020-07-23 13:01:15 -0400

    Add labels to game board

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
 const Board = () => {
-  return (
-    <div className="board">
-      {/* Output 100 squares */}
-      {Array(100).fill(<div className="square">square</div>)}
-    </div>
-  );
+  const generateBoard = () => {
+    const squares = [];
+    const numSquares = 11;
+    for (let row = 0; row < numSquares; ++row) {
+      for (let col = 0; col < numSquares; ++col) {
+        if (row === 0) {
+          if (col === 0) {
+            squares.push(<div className="square"> </div>);
+          } else {
+            squares.push(
+              <div className="square">{String.fromCharCode(64 + col)}</div>
+            );
+          }
+        } else {
+          if (col === 0) {
+            squares.push(<div className="square">{row}</div>);
+          } else {
+            squares.push(<div className="square">sq</div>);
+          }
+        }
+      }
+    }
+    return squares;
+  };
+
+  return <div className="board">{generateBoard()}</div>;
 };
 
 export default Board;
