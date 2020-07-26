dd4405c0f2ac71098af5ccbf29826592cc4c07d6
likithaaa <likithad1@gmail.com>
2020-07-26 18:41:24 -0400

    Delete commented code

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -18,36 +18,6 @@ const BurgerNav = ({ getSession, sessionUser }) => {
 
   // Render
   return (
-    // <nav>
-    //   <ul>
-    //     <li>Burger icon</li>
-    //     <li>
-    //       <Link to="/">Home</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/signup">Sign up</Link>
-    //     </li>
-    //     {sessionUser ? (
-    //       <>
-    //         <li>
-    //           <Link to="/profile">Profile</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/new/game">New game</Link>
-    //         </li>
-    //         <li>
-    //           <button type="button" onClick={destroySession}>
-    //             Logout
-    //           </button>
-    //         </li>
-    //       </>
-    //     ) : (
-    //       <li>
-    //         <Link to="/login">Login</Link>
-    //       </li>
-    //     )}
-    //   </ul>
-    // </nav>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="/">Battleship</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
