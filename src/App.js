52303a4d57929856f17669dfc74f23772f78d934
caldric <cvaldric@gmail.com>
2021-02-09 22:54:33 -0500

    Fix deployment API
    Remove extra '/' at the end of the URL.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Signup from './component/Signup';
 // Configuration
 const API = {
   local: 'http://localhost:8080',
-  deployment: 'https://calm-lake-66619.herokuapp.com/',
+  deployment: 'https://calm-lake-66619.herokuapp.com',
   index: 'battleship',
 };
 const baseURL =
